0150714fb32e3f514004c4aeeaeab53f1c1ce8de
Santiago del Giorgio <sdelgiorgio@minfra.gba.gob.ar>
2026-05-06 11:21:53 -0300

    rutas

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/catalogo', function () {
     return view('catalogo');
-});
+})->name('catalogo');
 
 Route::get('/ubicacion', function () {
     return view('ubicacion');
+})->name('ubicacion');
+
+Route::fallback(function () {
+    return redirect()->route('home');
 });
